2ef975f28d09ccf452764b50d840f95ea11646f9
Jose Renau <renau@ucsc.edu>
2021-11-06 09:27:52 -0700

    add fmt

--- WORKSPACE
+++ WORKSPACE
@@ -1,4 +1,5 @@
 load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 git_repository(
     name = "com_google_googletest",
@@ -28,4 +29,14 @@ git_repository(
     name = "com_github_gflags_gflags",
     remote = "https://github.com/gflags/gflags.git",
     tag = "v2.2.2",
+)
+
+http_archive(
+    name = "fmt",
+    build_file = "BUILD.fmt",
+    sha256 = "fccfc86e4aff6c33bff9a1d907b2ba2de2a5a8ab84349be4964a93f8d8c21b62",
+    strip_prefix = "fmt-7bdf0628b1276379886c7f6dda2cef2b3b374f0b",
+    urls = [
+        "https://github.com/fmtlib/fmt/archive/7bdf0628b1276379886c7f6dda2cef2b3b374f0b.zip",
+    ],
 )
--- gold/gold_test.cpp
+++ gold/gold_test.cpp
@@ -1,20 +1,28 @@
 
-#include <string>
+//#include "imli.hpp"
 
+#include <string>
+#include "fmt/format.h"
+
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-std::string GetHello(std::string_view in) {
-  if (in.size() == 0) {
-    return std::string("hello, world");
-  } else {
-    return std::string("Hello, ") + in.data() ;
+class Gold_test : public ::testing::Test {
+protected:
+  void SetUp() override {}
+
+  void TearDown() override {
+    // Graph_library::sync_all();
   }
+};
+
+TEST_F(Gold_test, Trivial_imli_test1) {
+
+  // instantiate IMLI, some configuration and test the basic API so that it it
+  // learns. It is also a way to showcase the API
+
+  fmt::print("hello world\n");
+
+  EXPECT_NE(10,100);
 }
 
-TEST(GetHello, CanMakeHelloWorldWithEmptyInput) {
-  EXPECT_EQ(GetHello(""), "hello, world");
-}
-
-TEST(GetHello, CanMakeHelloWithNonEmptyInput) {
-  EXPECT_EQ(GetHello("Ari"), "Hello, Ari");
-}
